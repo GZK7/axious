be2b66187e6825e5fb2a21ebf461a3f4992aa819
Your Name <717842419@qq.com>
2019-06-13 08:36:49 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+     
     </div>
     <router-view/>
+    
+     <table>
+       <tr>
+         <td>id</td>
+         <td>IMEI</td>
+         <td>device</td>
+         <td>message</td>
+         <td>tel</td>
+       </tr>
+       <tr v-for='index in syk'>
+          <td> {{index.id}}</td>
+       <td>{{index.IMEI}}</td>
+       <td> {{index.tel}}</td>
+          <td> {{index.message}}</td>
+    <td> {{index.tel}}</td>
+      
+       </tr>
+     </table>
+     <button @click="mu">55</button>
+     
+
+    
   </div>
 </template>
 
@@ -28,4 +49,55 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+table{
+    table-layout:fixed;
+    empty-cells:show; 
+    border-collapse: collapse;
+    margin:0 auto;
+    border: 2px solid black
+}
+td{
+  border: 1px solid black;
+    height:20px;
+}
 </style>
+<script>
+export default {
+
+  name: "app",
+  
+  
+
+  data(){
+    return{
+      syk:''
+    }
+  },
+
+
+// created(){
+//       this.axios.get('http://localhost:60020/all').then((response)=>{
+//         console.log(response.data.data[1]);
+//         this.syk=response.data.data;
+
+//         console.log(this.syk);
+//       }).catch((error)=>{
+//           console.log(error)
+//       })
+//     },
+    methods:{
+
+      mu(){
+      this.axios.get('http://localhost:60020/all').then((response)=>{
+        console.log(response.data.data[1]);
+        this.syk=response.data.data;
+
+        console.log(this.syk);
+      }).catch((error)=>{
+          console.log(error)
+      })
+    }
+    }
+  
+};
+</script>
